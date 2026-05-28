84afcc9a7075f044cbad59e461ebbdba5ba7517f
Md. Rifat Islam Rizvi <rifatrizviofficial001@gmail.com>
2026-05-28 06:04:16 +0600

    asgi change

--- proctor_ai/asgi.py
+++ proctor_ai/asgi.py
@@ -1,27 +1,21 @@
-"""
-ASGI config for proctor_ai project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proctor_ai.settings')
+
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 from ai_tutor.middleware import JwtAuthMiddlewareStack
 from proctor_ai.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proctor_ai.settings')
-
-django_asgi_app = get_asgi_application()
-
 application = ProtocolTypeRouter(
-	{
-		'http': django_asgi_app,
-		'websocket': JwtAuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
-	}
+    {
+        "http": django_asgi_app,
+        "websocket": JwtAuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        ),
+    }
 )
